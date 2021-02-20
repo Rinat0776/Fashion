e8802ad4cc51c2b37ced5fe95e04264b34406801
Rinat0776 <rinat0776802@gmail.com>
2021-02-20 14:10:39 +0300

    created menu window connected javascript to progect

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let modal = document.querySelector('.modal');
+let headerBars = document.querySelector('.header__bars');
+let menuClose = document.querySelector('.menu__close');
+
+function toggleMenu() {
+    modal.classList.toggle('hidden');
+}
+
+headerBars.addEventListener('click', toggleMenu);
+menuClose.addEventListener('click', toggleMenu);
